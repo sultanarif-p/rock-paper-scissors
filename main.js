5e3230bc2babb6935aee2baa0e7111645b4aa75a
sultan arif <53237271+sultanarif-p@users.noreply.github.com>
2020-08-28 01:00:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,66 @@
-const userChoice = userInput => {
+
+const getUserChoice = userInput => {
   userInput = userInput.toLowerCase();
-
-if (userInput === 'rock' || userInput === 'paper' || userInput === "scissors" ) {
-  return userInput;
-} else {
-  console.log('Error!');
-}
+ if( userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'|| userInput === 'bomb') {
+   return userInput; 
+ } else {
+   console.log('Error!');
+ }
 };
 
-
-const computerChoice = ()  =>  {
-  let randomNumber=Math.floor(Math.random() * 3);
-switch (randomNumber) {
-  case 0:
-    return 'rock';
-  case 1:
-    return 'paper';
-  case 2:
-      return 'scissors'
-}
-}
-
-
-const determineWinner =  () =>  {
-  if (userChoice === computerChoice) {
-  return 'The game is a tie!';
-}
-if (userChoice === 'rock') {
-  if (computerChoice === 'paper') {
-    return 'The computer won!';
-  } else {
-    return 'You won!';
+ function getComputerChoice(){
+   let randomNumber = Math.floor(Math.random() *3);
+    switch(randomNumber){
+      case 0:
+        return 'rock';
+      case 1:
+        return 'paper';
+      case 2:
+        return 'scissors';
+    }
   }
-}
-if (userChoice === 'scissors') {
-  if (computerChoice === 'rock') {
-    return 'The computer won!';
-  } else {
-    return 'You won!';
+
+ 
+function determineWinner(userChoice, computerChoice){
+  if(userChoice === 'bomb'){
+    return "You are the bomb and won."
   }
+  if(userChoice === computerChoice){
+    return "The game was a tie";
+  }
+  if(userChoice === 'rock'){
+    if(computerChoice === 'paper'){
+      return 'The computer won.';
+    } else {
+      return 'You won';
+    }//end else
+    }//end if userchoice = rock
+    if(userChoice === 'paper'){
+      if(computerChoice === 'scissors' || computerChoice === 'rock'){
+        return 'The computer won';
+      } else {
+        return 'You won';
+      }//end else
+    }//end if userchoice = paper
+ if(userChoice === 'scissors'){
+      if(computerChoice === 'rock' || computerChoice === 'paper'){
+        return 'The computer won';
+      } else {
+        return 'You won';
+      }//end else
+    }//end if userchoice = paper
 }
-}
-
-
-console.log(determineWinner('paper', 'scissors')); // prints something like 'The computer won!'
-console.log(determineWinner('paper', 'paper')); // prints something like 'The game is a tie!'
-console.log(determineWinner('paper', 'rock')); // prints something like 'The user won!'
-
-
 const playGame = () => {
-   const userChoice = userChoice('scissors');
-   const computerChoice = computerChoice();
+  const userChoice = getUserChoice( 'bomb');
+  console.log(userChoice);
+  const computerChoice = getComputerChoice();
    console.log('You threw: ' + userChoice);
-   console.log('The computer threw:' + computerChoice);
-   console.log(determineWinner(userChoice, computerChoice));
+  console.log('The computer threw: ' + computerChoice);
+console.log(determineWinner(userChoice, computerChoice));
 };
-
 playGame();
+
+
+
+
+ 
